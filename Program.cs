8616591867bafe093121700a9e743bb6acc3e492
AdeyBoy123 <groveraden@gmail.com>
2023-09-14 16:37:38 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int digit;
+
+            while (number > 0)
+            {
+                digit = number % 10;
+                sum += digit;
+                number /= 10;
+            }
+
+            Console.WriteLine("The sum of the digits is: {0}", sum);
+        }
+    }
+}
